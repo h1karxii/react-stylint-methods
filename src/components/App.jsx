e85b830a4f2dcfb5bc32d3628c7e6e8fb5866876
h1karxii <h1karxii@gmail.com>
2023-04-03 07:06:19 +0800

    feat: set up styled-components and example code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,77 @@
-import logo from 'assets/logo.svg'
-import './App.css'
+import { useState } from 'react'
+import styled from 'styled-components'
+import { ReactComponent as Logo } from 'assets/logo.svg'
+
+const Styles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  font-size: calc(10px + 2vmin);
+  color: #fff;
+  text-align: center;
+  background-color: #282c34;
+
+  .app__logo {
+    height: 40vmin;
+    pointer-events: none;
+
+    &--spin {
+      animation: ${(props) =>
+          props.isClockwise ? 'clockwise' : 'counterclockwise'}
+        infinite 20s linear;
+    }
+  }
+
+  .app__button {
+    width: 200px;
+    height: 50px;
+    font-size: 20px;
+    font-weight: 800;
+    color: #282c34;
+  }
+
+  @keyframes clockwise {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes counterclockwise {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(-360deg);
+    }
+  }
+`
 
 function App() {
+  const [isClockwise, setIsClockwise] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Styles isClockwise={isClockwise}>
+      <Logo className="app__logo app__logo--spin" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <button
+        type="button"
+        className="app__button"
+        onClick={() => {
+          setIsClockwise(!isClockwise)
+        }}
+      >
+        Turn around
+      </button>
+    </Styles>
   )
 }
 
